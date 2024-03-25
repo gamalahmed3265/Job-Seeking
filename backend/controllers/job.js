1597c4518f0fb272228850d6b327ee96507d3fb2
Gamal Ahmed <gamalahmed3265@gmail.com>
2024-03-25 16:27:24 +0200

    finish Controller job add ,update and deleted

--- backend/controllers/job.js
+++ backend/controllers/job.js
@@ -16,8 +16,127 @@ export const getAllJobs=catchAsyncError(async (req,res,next)=>{
 })
 
 export const postJob=catchAsyncError(async (req,res,next)=>{
-    console.log(req.user);
+    const {role,_id}=req.user;
+    if(role==="Job Seeke"){
+        return next(
+            new ErrorHandler("Job Seeke is Not Allowed access to resource",400)
+        )
+    }
+
+    const {title,
+        description,
+        category,
+        country,
+        city,
+        location,
+        fixedSalary,
+        salaryFrom,
+        salaryTo,
+    }=req.body;
+
+    if( !title||
+        !description||
+        !category||
+        !country||
+        !city||
+        !location){
+            return next(
+                new ErrorHandler("Please Provide Full Job Details",400)
+            )
+    }
+    if((!salaryFrom||!salaryTo) && !fixedSalary){
+            return next(
+                ErrorHandler("Please either provide fixed Salary or ranged salary")
+            )
+    }
+    if(salaryFrom && salaryTo && fixedSalary){
+            return next(
+                new ErrorHandler("Connt enter fixed and ranged Salary Together!")
+            )
+    }
+    
+    const job=await JobModel.create({
+        title,
+        description,
+        category,
+        country,
+        city,
+        location,
+        fixedSalary,
+        salaryFrom,
+        salaryTo,
+        postedBy:_id
+    })
     res.json({
-        req:req.body
+        success:true,
+        message:"Job Posted Successfully!",
+        job
     })
 })
+
+export const getMyJobs=catchAsyncError(async(req,res,next)=>{
+    const {role,_id}=req.user;
+    if(role==="Job Seeke"){
+        return next(
+            new ErrorHandler("Job Seeke is Not Allowed access to resource",400)
+        )
+    }
+    const myJobs=await JobModel.find({
+        postedBy:req.user._id
+    });
+    res.status(200).json({
+        success:true,
+        myJobs,
+    })
+});
+export const updateJob=catchAsyncError(async(req,res,next)=>{
+    const {id}=req.params;
+
+    const {role}=req.user;
+    if(role==="Job Seeke"){
+        return next(
+            new ErrorHandler("Job Seeke is Not Allowed access to resource",400)
+        )
+    }
+    const myJob=await JobModel.findById(id);
+
+    if(!myJob){
+        return next(
+            new ErrorHandler("Oops job Not found!",400))
+}
+
+    const job=await JobModel.findByIdAndUpdate(id,req.body,{
+        new:true,
+        runValidators:true,
+        userFindAndModify:false
+    })
+
+    res.status(200).json({
+        success:true,
+        message:"Job Updated Succesfuly",
+        job,
+    })
+});
+export const DeleteJob=catchAsyncError(async(req,res,next)=>{
+    const {id}=req.params;
+
+    const {role}=req.user;
+    if(role==="Job Seeke"){
+        return next(
+            new ErrorHandler("Job Seeke is Not Allowed access to resource",400)
+        )
+    }
+    const myJob=await JobModel.findById(id);
+
+    if(!myJob){
+        return next(
+            new ErrorHandler("Oops job Not found!",400))
+}
+
+    await myJob.deleteOne();
+
+    res.status(200).json({
+        success:true,
+        message:"Job Deleted Succesfuly",
+    })
+});
--- backend/route/jobRouter.js
+++ backend/route/jobRouter.js
@@ -1,10 +1,13 @@
 import express from "express"
-import { getAllJobs, postJob } from "../controllers/job.js";
+import { DeleteJob, getAllJobs, getMyJobs, postJob, updateJob } from "../controllers/job.js";
 import { isAuthorized } from "../middlewares/auth.js";
 
 const router=express.Router();
 
 router.get("/getAll",getAllJobs)
 router.post("/postJob",isAuthorized,postJob)
+router.get("/getMyJobs",isAuthorized,getMyJobs)
+router.put("/updateJob/:id",isAuthorized,updateJob)
+router.delete("/deleteJob/:id",isAuthorized,DeleteJob)
 
 export default router;
