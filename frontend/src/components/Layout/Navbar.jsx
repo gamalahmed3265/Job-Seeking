5be18e94856cb18a42e1d3d28eb5862a68a46814
Gamal Ahmed <gamalahmed3265@gmail.com>
2024-04-13 17:44:58 +0200

    add section Home

--- frontend/src/components/Layout/Navbar.jsx
+++ frontend/src/components/Layout/Navbar.jsx
@@ -13,13 +13,13 @@ const Navbar = () => {
   const handleLogout = async () => {
     try {
       const response = await axios.get(
-        "http://localhost:4000/api/v1/user/logout",
+        "http://localhost:4000/api/v1/user/logout/",
         {
           withCredentials: true,
         }
       );
-      toast.success(response.data.message);
-      setIsAuthorized(false);
+      // toast.success(response.data.message);
+      setIsAuthorized(true);
       navigateTo("/login");
     } catch (error) {
       toast.error(error.response.data.message), setIsAuthorized(true);
